bdbb1ece4ede6d3eb9a5a6b849314902d5637f53
matthieu <decker.matthieu@gmail.com>
2023-04-08 15:20:37 -0400

    working

--- src/main/java/com/techelevator/VendingMachine.java
+++ src/main/java/com/techelevator/VendingMachine.java
@@ -1,4 +1,58 @@
 package com.techelevator;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.List;
+import com.techelevator.view.*;
+import org.w3c.dom.ls.LSOutput;
 
 public class VendingMachine {
+    File vendor = new File("vendingmachine.csv");
+    List<Product> inventory = new ArrayList<>();
+    try (Scanner readInventory = new Scanner(vendor)){
+        while (readInventory.hasNextLine()) {
+            String line = readInventory.nextLine();
+            String[] attributes = line.split("|");
+            this.makeInventoryItem(attributes);
+        }
+    }    catch (FileNotFoundException e){
+        System.out.println("error");
+    }
+
+
+
+   /* public String getType(String[] atts){
+        List<String> types = new ArrayList<>();
+        types.add("Candy");
+        types.add("Drink");
+        types.add("Gum");
+        types.add("Chip");
+        for(String s : types){
+            if(atts[3].equals(s)){
+                return s;
+            }
+        }
+    }*/
+
+    public void makeInventoryItem(String[] atts){
+        if(atts[3].equals("Candy")){
+            Product c = new Candy(atts[2], atts[1], atts[0]);
+            inventory.add(c);
+        }
+        else if (atts[3].equals("Drink")){
+            Product c = new Drink(atts[2], atts[1], atts[0]);
+            inventory.add(c);
+        }
+        else if (atts[3].equals("Chip")){
+            Product c = new Chips(atts[2], atts[1], atts[0]);
+            inventory.add(c);
+        }
+        else if (atts[3].equals("Gum")){
+            Product c = new Gum(atts[2], atts[1], atts[0]);
+            inventory.add(c);
+        }
+    }
+
+
 }
