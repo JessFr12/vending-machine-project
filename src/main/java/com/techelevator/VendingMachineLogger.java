5afbc8d9ef4ea21880f2edce2c7dd338b905fb7d
Matthew <mattsimmonds74@gmail.com>
2023-04-09 16:21:14 -0400

    VendingMachineLogger Progress 2

--- src/main/java/com/techelevator/VendingMachineLogger.java
+++ src/main/java/com/techelevator/VendingMachineLogger.java
@@ -1,27 +1,31 @@
 package com.techelevator;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
+import java.io.*;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class VendingMachineLogger {
-    private PrintWriter writer;
-    public VendingMachineLogger() throws FileNotFoundException {
-        this.writer = new PrintWriter(new File("Log.txt"));
+    private  PrintWriter writer;
+    public VendingMachineLogger() throws IOException {
+//        the second true auto filled not completely sure why
+        writer = new PrintWriter(new FileWriter("Log.txt", true), true);
     }
 
     public void logTransaction(String action, double amount, double balance) {
         LocalDateTime timestamp = LocalDateTime.now();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyy hh:mm:ss");
         String formattedTimestamp = timestamp.format(formatter);
+        writer.println(formattedTimestamp + " " + action + ": $" + amount + " $" + balance);
     }
     public void logFeedMoney(double amount, double balance) {
         logTransaction("Feed Money", amount, balance);
     }
-
-
+    public void logPurchase(String id, double amount, double balance) {
+        logTransaction(id, amount, balance);
+    }
+    public void logGiveChange(double amount, double balance) {
+        logTransaction("Give Change", amount, balance);
+    }
 
 }
 
